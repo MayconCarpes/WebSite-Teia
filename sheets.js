c1b84f420f13cbd23cbe23befe60bc5141f9002a
Maycon Luis Franco Carpes <36649863858@campus.machado.mch>
2025-01-27 21:41:30 -0300

    google sheets

---
+++ sheets.js
@@ -0,0 +1,24 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+
+    const formData = new FormData(this);
+    const data = Object.fromEntries(formData.entries());
+
+    fetch('https://script.google.com/macros/s/AKfycbx8x56601d0Pi0StETqosEQZHnUM25l7w6qwse5zQgDoiBIIJUlzsUtBxrwQrHitlvG/exec', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.result === "success") {
+            document.getElementById('notification').style.display = 'block'; // Exibe notificação
+            this.reset(); // Limpa o formulário
+        } else {
+            console.error('Erro:', data.message);
+        }
+    })
+    .catch(error => console.error('Erro:', error));
+});
