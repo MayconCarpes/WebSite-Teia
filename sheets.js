1bde7b128c7cc62b9e19087d9703371aa2ad67e9
Maycon Luis Franco Carpes <36649863858@campus.machado.mch>
2025-01-27 21:59:12 -0300

    google sheets v1.1

--- sheets.js
+++ sheets.js
@@ -1,24 +1,26 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Impede o envio padrão do formulário
 
-    const formData = new FormData(this);
-    const data = Object.fromEntries(formData.entries());
+        const formData = new FormData(this);
+        const data = Object.fromEntries(formData.entries());
 
-    fetch('https://script.google.com/macros/s/AKfycbyz65XyHWkLAQMJZBOS6YrKHsSN-Nhk8UZCVfQ7WWSXfF1ct1KlHSiaWWBiwArl1vgT/exec', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.result === "success") {
-            document.getElementById('notification').style.display = 'block'; // Exibe notificação
-            this.reset(); // Limpa o formulário
-        } else {
-            console.error('Erro:', data.message);
-        }
-    })
-    .catch(error => console.error('Erro:', error));
+        fetch('https://script.google.com/macros/s/AKfycbyz65XyHWkLAQMJZBOS6YrKHsSN-Nhk8UZCVfQ7WWSXfF1ct1KlHSiaWWBiwArl1vgT/exec', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.result === "success") {
+                document.getElementById('notification').style.display = 'block'; // Exibe notificação
+                this.reset(); // Limpa o formulário
+            } else {
+                console.error('Erro:', data.message);
+            }
+        })
+        .catch(error => console.error('Erro:', error));
+    });
 });
