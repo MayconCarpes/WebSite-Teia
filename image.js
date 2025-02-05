9b199e44fb4fe27d0e52bab9b4247f9d2d821d5d
maycon <mayconcarpes541@gmail.com>
2025-02-05 00:01:01 -0300

    atualização

--- image.js
+++ image.js
@@ -1,19 +1,21 @@
 function abrirImagem(src) {
     var modal = document.getElementById("imagem-modal");
     var imagemExpandida = document.getElementById("imagem-expandida");
-    var barraNavegacao = document.getElementById("barra-navegacao");
-    
+    var barraNavegacao = document.querySelector(".navbar"); // Pegando a navbar do Bootstrap
+
     imagemExpandida.src = src;
     modal.style.display = "block";
-    barraNavegacao.style.position = "absolute"; // Muda a posição para absolute
-    barraNavegacao.style.top = "100px"; // Ajuste a posição para baixo da imagem
+
+    // Esconder a barra de navegação ao abrir a imagem
+    barraNavegacao.style.display = "none"; 
 }
 
 function fecharImagem() {
     var modal = document.getElementById("imagem-modal");
-    var barraNavegacao = document.getElementById("barra-navegacao");
-    
+    var barraNavegacao = document.querySelector(".navbar");
+
     modal.style.display = "none";
-    barraNavegacao.style.position = "fixed"; // Restaura a posição para fixed
-    barraNavegacao.style.top = "0"; // Restaura a posição original
+
+    // Mostrar a barra de navegação novamente
+    barraNavegacao.style.display = "flex"; // Restaurar a exibição da navbar
 }
